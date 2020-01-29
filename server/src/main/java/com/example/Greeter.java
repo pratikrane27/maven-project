9476bf02a481d3ea78fedcbd118411532d201436
amit.fegade121@gmail.com <amit.fegade121@gmail.com>
2020-01-29 16:10:13 +0530

    Fixed javadoc comment warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   *@param name of the person
-   *@return Hello message
+   * @param name of the person
+   * @return Hello message
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
